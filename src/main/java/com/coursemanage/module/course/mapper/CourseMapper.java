74094eec9c6e3a5bd465a6eb4a09d64864c32397
wenqiuyi <wenqiuyi2024@163.com>
2025-12-05 14:57:11 +0800

    Update CourseMapper.java

--- src/main/java/com/coursemanage/module/course/mapper/CourseMapper.java
+++ src/main/java/com/coursemanage/module/course/mapper/CourseMapper.java
@@ -2,10 +2,23 @@ package com.coursemanage.module.course.mapper;
 
 import com.coursemanage.module.course.pojo.Course;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import java.util.List;
+
 
 @Mapper
 public interface CourseMapper {
-    List<Course> getCoursesByFilter(String teacherNo, String acaYear, String semester);
+    @Select("SELECT id, name, description, " +
+            "comment_area AS commentArea, " +
+            "aca_year AS acaYear, " +
+            "semester, teacher_no AS teacherNo " +
+            "FROM course")
+    List<Course> getCourses();
+
+    @Select("SELECT id, name, description, " +
+            "comment_area AS commentArea, " +
+            "aca_year AS acaYear, " +
+            "semester, teacher_no AS teacherNo " +
+            "FROM course WHERE id = #{id}")
     Course getCourseById(Integer id);
 }
