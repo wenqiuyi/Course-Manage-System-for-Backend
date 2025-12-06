511271ecea116cc7da701b4f19b410678e40add0
wenqiuyi <wenqiuyi2024@163.com>
2025-12-06 17:44:48 +0800

    update

--- src/main/java/com/coursemanage/module/course/mapper/CourseMapper.java
+++ src/main/java/com/coursemanage/module/course/mapper/CourseMapper.java
@@ -6,26 +6,19 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
-
 @Mapper
 public interface CourseMapper {
-   @Select("SELECT id, name, description, " +
-        "comment_area AS commentArea, " +
-        "aca_year AS acaYear, " +
-        "semester, teacher_no AS teacherNo " +
-        "FROM course " +
-        "WHERE teacher_no = #{teacherNo} " +
-        "AND aca_year = #{acaYear} " +
-        "AND semester = #{semester}")
-List<Course> getCourses(@Param("teacherNo") String teacherNo,
-                        @Param("acaYear") String acaYear,
-                        @Param("semester") String semester);
-
+    @Select("SELECT id, name, description, " +
+            "comment_area AS commentArea, " +
+            "aca_year AS acaYear, " +
+            "semester, teacher_no AS teacherNo " +
+            "FROM course WHERE teacher_no = #{teacherNo}")
+    List<Course> getCourses(@Param("teacherNo") String teacherNo);
 
     @Select("SELECT id, name, description, " +
             "comment_area AS commentArea, " +
             "aca_year AS acaYear, " +
             "semester, teacher_no AS teacherNo " +
             "FROM course WHERE id = #{id}")
-    Course getCourseById(Integer id);
+    Course getCourseById(@Param("id") Integer id);
 }
