6322f07439e9c6b43f04231d7f81affe5cf990d2
wenqiuyi <wenqiuyi2024@163.com>
2025-12-07 14:13:28 +0800

    update

--- src/main/java/com/coursemanage/module/course/pojo/Course.java
+++ src/main/java/com/coursemanage/module/course/pojo/Course.java
@@ -14,7 +14,7 @@ public class Course {
     // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     // 不注释会报错，有待处理
-    // private Integer classId; // 可选
+    //private Integer classId; // 可选
     private String name;
     private String description;
     private Boolean commentArea;
@@ -22,12 +22,5 @@ public class Course {
     private String semester;
     private String teacherNo;
 
-    public void setTeacher(Teacher t) {
 
-    }
-    // 当返回课程详情时，可填充 teacher 信息（组合）
-    // 不加注解会报错，这里ai的做法请自行斟酌
-    // 修复：添加@Transient注解，表示不映射到数据库
-    // @Transient
-    // private Teacher teacher;
 }
