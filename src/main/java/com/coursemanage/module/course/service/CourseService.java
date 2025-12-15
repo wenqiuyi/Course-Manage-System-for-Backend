548c08950adf7f4d1172db25fac2cf4085d29d6b
wenqiuyi <wenqiuyi2024@163.com>
2025-12-15 16:55:29 +0800

    增加和修改部分接口

--- src/main/java/com/coursemanage/module/course/service/CourseService.java
+++ src/main/java/com/coursemanage/module/course/service/CourseService.java
@@ -1,5 +1,6 @@
 package com.coursemanage.module.course.service;
 
+import com.coursemanage.module.course.dto.CourseUpdateDTO;
 import com.coursemanage.module.course.pojo.Course;
 import java.util.List;
 
@@ -7,4 +8,6 @@ public interface CourseService {
     List<Course> getCourses(String teacherNo);
     List<Course> getAllCourses();
     Course getCourseById(Integer id);
+
+    boolean updateCourse(Integer id, CourseUpdateDTO dto);
 }
