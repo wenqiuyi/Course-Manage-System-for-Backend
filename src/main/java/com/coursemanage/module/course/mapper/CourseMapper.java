82cc9aab23065bdab390bcca84ac2291d8095924
wenqiuyi <wenqiuyi2024@163.com>
2025-12-15 16:54:45 +0800

    增加和修改部分接口

--- src/main/java/com/coursemanage/module/course/mapper/CourseMapper.java
+++ src/main/java/com/coursemanage/module/course/mapper/CourseMapper.java
@@ -4,6 +4,8 @@ import com.coursemanage.module.course.pojo.Course;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 import java.util.List;
 
 @Mapper
@@ -14,7 +16,6 @@ public interface CourseMapper {
             "semester, teacher_no AS teacherNo " +
             "FROM course WHERE teacher_no = #{teacherNo}")
     List<Course> getCourses(@Param("teacherNo") String teacherNo);
-    
     @Select("SELECT id, name, description, " +
             "comment_area AS commentArea, " +
             "aca_year AS acaYear, " +
@@ -28,4 +29,17 @@ public interface CourseMapper {
             "semester, teacher_no AS teacherNo " +
             "FROM course WHERE id = #{id}")
     Course getCourseById(@Param("id") Integer id);
+
+    @Update("""
+        UPDATE course
+        SET
+        name = #{name},
+        description = #{description},
+        comment_area = #{commentArea},
+        aca_year = #{acaYear},
+        semester = #{semester},
+        teacher_no = #{teacherNo}
+        WHERE id = #{id}
+        """)
+    int updateById(Course course);
 }
