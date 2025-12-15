783b883f578ab97416f6e1bc716de3ff7ef33003
wenqiuyi <wenqiuyi2024@163.com>
2025-12-15 16:50:32 +0800

    增加删除接口

--- src/main/java/com/coursemanage/module/comment/mapper/CommentMapper.java
+++ src/main/java/com/coursemanage/module/comment/mapper/CommentMapper.java
@@ -9,14 +9,16 @@ import java.util.List;
 public interface CommentMapper {
 
     @Select("""
-        SELECT id,
-               course_id AS courseId,
-               commenter_no AS commenterNo,
-               content
-        FROM course_comment
-        WHERE course_id = #{courseId}
-        """)
+    SELECT id,
+           course_id AS courseId,
+           commenter_no AS commenterNo,
+           content,
+           create_time AS createTime
+    FROM course_comment
+    WHERE course_id = #{courseId}
+    """)
     List<CourseComment> getCommentsByCourseId(@Param("courseId") Integer courseId);
+
 
     @Insert("""
         INSERT INTO course_comment(course_id, commenter_no, content)
@@ -24,4 +26,11 @@ public interface CommentMapper {
         """)
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insertComment(CourseComment comment);
+
+    @Delete("""
+    DELETE FROM course_comment
+    WHERE id = #{id}
+    """)
+    void deleteComment(@Param("id") Integer id);
+
 }
